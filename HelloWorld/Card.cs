ec05ba3f9482cc5fa9d6732543ee140aa04265c8
bleenie <117716131+bleenie@users.noreply.github.com>
2024-04-15 22:27:06 +0200

    Made class files

---
+++ HelloWorld/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class Card
+    {
+        int suit;
+        int rank;
+        int value;
+
+        public Card(int value, int rank, int suit)
+        {
+            this.value = value;
+            this.rank = rank;
+            this.suit = suit;
+        }
+
+        public int getSuit() {
+            return suit;
+        }
+
+        public int getRank() { 
+            return rank;    
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+    }
+}
